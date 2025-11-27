ed2e6c30acd657e49060f2d8c6a50fcbb6942fe0
camilagrabowski-lgtm <camila.grabowski@aluno.senai.br>
2025-11-27 16:15:48 -0300

    Add files via upload

---
+++ exercicios C/func02.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int quad(int x);
+
+int main(){
+
+  setlocale(LC_ALL, "Portuguese");
+
+  int num, res;
+
+  printf("Digite um número: ");
+  scanf(" %d", &num);
+
+  res = quad(num);
+  printf("O quadrado de %d é: %d", num, res);
+
+  system("pause");
+  return 0;
+
+}
+
+int quad(int x){
+    int quadrado;
+
+    quadrado = x*x;
+    return quadrado;
+}
